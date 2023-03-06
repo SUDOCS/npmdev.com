eaf7505a4a068c17fb491d828a16114fa8f01843
SUDOCS <admin@imhcl.cn>
2023-03-06 19:06:37 +0800

    ci: compress output before uploading to server

--- scripts/tgz.ts
+++ scripts/tgz.ts
@@ -1,11 +1,22 @@
+import fs from 'fs'
 import path, { resolve } from 'path'
 import compressing from 'compressing'
 
 path.join = path.posix.join
 
-const input = resolve(__dirname, '../.output/')
-const output = resolve(__dirname, '../output.tgz')
+const tgzStream = new compressing.tgz.Stream()
 
-compressing.tgz.compressDir(input, output).then(() => {
-  console.log('compressed successfully')
-})
+tgzStream.addEntry(resolve(__dirname, '../.output/'))
+tgzStream.addEntry(resolve(__dirname, '../ecosystem.config.js'))
+
+fs.mkdirSync(resolve(__dirname, '../.deploy'), { recursive: true })
+
+const destStream = fs.createWriteStream(resolve(__dirname, '../.deploy/output.tgz'))
+
+tgzStream.pipe(destStream)
+  .on('finish', () => {
+    console.log('compress finished')
+  })
+  .on('error', (err) => {
+    console.error('compress failed', err)
+  })
