08fd280c70cff4ae7fdc152af65390dc621df0d4
SUDOCS <admin@imhcl.cn>
2023-03-16 18:01:39 +0800

    feat: member list and chat interface

--- pages/live/index.vue
+++ pages/live/index.vue
@@ -5,8 +5,23 @@ const { roomId, senderId, roomUserIds, wsStatus, pushing, pulling, openWebsocket
   videoEle,
 })
 
+const showMember = ref(false)
+const showChat = ref(false)
+const showSidebar = computed(() => showMember.value || showChat.value)
+
 function generateRoomId() {
   roomId.value = Math.random().toString(10).slice(2, 8)
+}
+
+function toggleSidebar(val: 'member' | 'chat') {
+  if (val === 'member') {
+    showMember.value = !showMember.value
+    showChat.value = false
+  }
+  else {
+    showChat.value = !showChat.value
+    showMember.value = false
+  }
 }
 </script>
 
@@ -58,19 +73,43 @@ function generateRoomId() {
         </div>
 
         <div v-show="pushing || pulling" absolute-full flex-center>
-          <video ref="videoEle" w-full h-full autoplay />
+          <video ref="videoEle" w-full h-full autoplay controls />
         </div>
       </div>
 
       <!-- 侧边栏 -->
 
-      <div class="live-side">
-        <div>
-          成员
+      <div v-show="showSidebar" class="live-side" relative>
+        <!-- 成员 -->
+        <div v-show="showMember" h-full overflow-y-auto>
+          <div v-for="x in 20" :key="x" frow gap-xs px-3 py-2 border="~ #eee b-solid" last="border-none">
+            <div rounded="1/2" w-12 h-12 lh-12 text-center bg-orange:10 border="~ #ddd solid">
+              <span>{{ (x + 100).toString().slice(0, 3) }}</span>
+            </div>
+            <div>
+              14321432
+            </div>
+          </div>
         </div>
+        <!-- 聊天 -->
+        <div v-show="showChat" fcol h-full>
+          <!-- 聊天记录 -->
 
-        <div>
-          聊天
+          <div flex-1 w-full>
+            12
+          </div>
+          <!-- 输入区域 -->
+
+          <div h-20 w-full relative border="~ #ddd t-solid">
+            <textarea name="chatInput" rows="10" border-none w-full h-full outline-none p-xs />
+
+            <div
+              absolute right-4 bottom-4 px-4 py-1 border-none outline-none text-xs
+              bg-black:20 rounded-sm cursor-pointer hover:bg-black:30 transition
+            >
+              发送
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -84,11 +123,11 @@ function generateRoomId() {
         <Icon name="fluent:dual-screen-desktop-24-regular" class="live-bottom-button-icon" />
         <span>分享屏幕</span>
       </div>
-      <div class="live-bottom-button">
+      <div class="live-bottom-button" @click="toggleSidebar('member')">
         <Icon name="fluent:people-24-regular" class="live-bottom-button-icon" />
         <span>成员</span>
       </div>
-      <div class="live-bottom-button">
+      <div class="live-bottom-button" @click="toggleSidebar('chat')">
         <Icon name="fluent:chat-24-regular" class="live-bottom-button-icon" />
         <span>沟通</span>
       </div>
@@ -109,7 +148,7 @@ function generateRoomId() {
 .live-bottom-bar{
   @apply w-full h-16 bottom-0 left-0 right-0 py-1 bg-white shadow
   border border-#eee border-t-solid
-  frow justify-center gap-xl;
+  frow justify-center gap-xl select-none;
 
   .live-bottom-button{
     @apply fcol text-xs;
